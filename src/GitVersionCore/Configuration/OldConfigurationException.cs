1bbd7a0679bd41ec39d5023c695c867186d3aa61
Andreas Heider <andreas@heider.io>
2016-07-03 16:34:14 +0100

    Don't use [Serializable] on netstandard

--- src/GitVersionCore/Configuration/OldConfigurationException.cs
+++ src/GitVersionCore/Configuration/OldConfigurationException.cs
@@ -1,19 +1,25 @@
 namespace GitVersion
 {
     using System;
+#if !NETSTANDARD
     using System.Runtime.Serialization;
+#endif
 
+#if !NETSTANDARD
     [Serializable]
+#endif
     public class OldConfigurationException : Exception
     {
         public OldConfigurationException(string message) : base(message)
         {
         }
 
+#if !NETSTANDARD
         protected OldConfigurationException(
             SerializationInfo info,
             StreamingContext context) : base(info, context)
         {
         }
+#endif
     }
 }
